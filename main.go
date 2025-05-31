79f495ab6c20828b9e4cf821c26574a47d513a63
Kavinda Gayashan <wkg@tiqri.com>
2025-05-31 14:01:08 +0530

    Implement graceful shutdown

--- main.go
+++ main.go
@@ -4,17 +4,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"time"
+
 	"leaderboard/pkg/api"
 )
 
 func main() {
+
 	server := &http.Server{
 		Addr:    ":8080", // TODO: Conmfigure port from environment variable or config file
 		Handler: api.Router(),
 	}
 
-	log.Println("Starting server on port ", server.Addr)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	go func() {
+		log.Println("Starting server on port ", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+		
+		log.Println("Server stopped gracefully")
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // TODO: Configure graceful shutdown timeout
+	defer cancel()
+	_ = server.Shutdown(ctx)
 }
